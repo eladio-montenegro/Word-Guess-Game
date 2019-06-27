a0f3451943f6e25342c2eed0bddd482e3bff5a02
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-06-27 18:17:29 -0500

    created the array with my word options

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+var word = ["leonardo", "michelangelo", "donatelo", "raphael", "splinter", "schredder", "bebop", "rocksteady", "april", "thefoot", "karai"]
+
+//function that collects key strokes
 document.onkeyup = function(event) {
     var userGuess = event.key;
 console.log(userGuess);
