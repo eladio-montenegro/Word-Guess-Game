d0d32a730c5154dab11e84b23bdcad4883d04829
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-06-28 14:15:44 -0500

    doing another push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,33 @@
+//word that can be guessed
 var word = ["leonardo", "michelangelo", "donatelo", "raphael", "splinter", "schredder", "bebop", "rocksteady", "april", "thefoot", "karai"]
+
+//computer chooses a word from the word array and save it as a variable
+    //word choosen at random 
+    function pickrandomword() {
+        return Math.floor(Math.random() * Math.floor(11));
+      }
+      console.log(pickrandomword());
+
+      console.log(word[4]);
+      var randomWord = word[Math.floor(Math.random() * Math.floor(11))];
+      console.log(randomWord);
+
+//compare keys pressed to letter in the stored word (var)
+
+//display letter that the user has guessed correctly and incorrectly 
+
+//user has 15 guesses
+
+//if no more guesses you lose the round and display you lose. point count
+
+//gamne resrtarts
+
+//
+
+
 
 //function that collects key strokes
 document.onkeyup = function(event) {
-    var userGuess = event.key;
+    var userGuess = event.key.toLocaleLowerCase();
 console.log(userGuess);
 }
